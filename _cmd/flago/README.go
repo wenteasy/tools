aa2f0e316b3d8dd8f51be1a16e891860f73b7bb8
secondarykey <secondarykey@gmail.com>
2021-11-10 18:47:43 +0900

    doc: add README

---
+++ _cmd/flago/README.go
@@ -0,0 +1,9 @@
+# flago
+
+The flago command is a command that outputs a constant for flag management with one variable.
+
+# Install 
+
+```
+  go install github.com/wenteasy/tools/_cmd/flago@latest
+```
